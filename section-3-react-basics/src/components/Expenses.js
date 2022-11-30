42f1b05fca594236f3f35e62409bded7efd6d000
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-30 17:01:30 +0200

    Lesson 39

--- section-3-react-basics/src/components/Expenses.js
+++ section-3-react-basics/src/components/Expenses.js
@@ -1,8 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './Expenses.css';
 
 function Expesnes(props) {
-  return <div className="expenses">
+  return (
+  <Card className="expenses">
     <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -23,7 +25,7 @@ function Expesnes(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-  </div>
+  </Card>)
 }
 
 export default Expesnes;
