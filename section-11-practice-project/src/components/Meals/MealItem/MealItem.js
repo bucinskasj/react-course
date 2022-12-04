374a406144994ef50c014cc1b2bae1dcc5957de6
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-04 16:16:16 +0200

    Lesson 136

--- section-11-practice-project/src/components/Meals/MealItem/MealItem.js
+++ section-11-practice-project/src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styles from './MealItem.module.css';
+import MealItemForm from "./MealItemForm";
 
 const MealItem = props => {
   const price = `$${props.price.toFixed(2)}`;
@@ -14,7 +15,7 @@ const MealItem = props => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm/>
       </div>
     </li>
   );
