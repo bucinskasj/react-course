67bc4f7586893e898bbba796a27da6e0b13ce2f0
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-05 19:33:02 +0200

    Removin DB URLs. Resources deleted as well

--- section-14-http-and-react/src/App.js
+++ section-14-http-and-react/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://react-http-ded7d-default-rtdb.firebaseio.com/movies.json');
+      const response = await fetch('LINK');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -44,7 +44,7 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await fetch('https://react-http-ded7d-default-rtdb.firebaseio.com/movies.json', {
+    const response = await fetch('LINK', {
       method: 'POST',
       body: JSON.stringify(movie),
       headers: {
