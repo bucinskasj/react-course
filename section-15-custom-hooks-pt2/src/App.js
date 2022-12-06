2581455976119175c30487b9e4448b05f81ba4fc
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-06 10:49:32 +0200

    lesson 193

--- section-15-custom-hooks-pt2/src/App.js
+++ section-15-custom-hooks-pt2/src/App.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        ''
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
+    setTasks(loadedTasks);
   };
+
+  const { isLoading, error, sendRequest: fetchTasks} = useHttp(
+    { url: "" },
+    transformTasks
+  );
 
   useEffect(() => {
     fetchTasks();
