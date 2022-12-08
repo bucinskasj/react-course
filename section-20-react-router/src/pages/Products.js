d1d7812ceeb796bd28efcbdc3488ffc8fb5e6e2b
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-08 19:20:25 +0200

    Lesson 273

--- section-20-react-router/src/pages/Products.js
+++ section-20-react-router/src/pages/Products.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>Products page</h1>
-      <li>Book</li>
-      <li>Carpet</li>
-      <li>Online Course</li>
+      <li><Link to='/products/p1'>Book</Link></li>
+      <li><Link to='/products/p2'>Carpet</Link></li>
+      <li><Link to='/products/p3'>Online Course</Link></li>
     </section>
   );
 };
