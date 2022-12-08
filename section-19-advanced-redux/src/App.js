96a972ba5f4737eb3e3a04e06dc28dac0d64f16b
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-08 08:19:38 +0200

    Lesson 256

--- section-19-advanced-redux/src/App.js
+++ section-19-advanced-redux/src/App.js
@@ -1,11 +1,19 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  useEffect(() => {
+    fetch("/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
