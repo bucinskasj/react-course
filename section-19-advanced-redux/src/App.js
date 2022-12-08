83e05b773a1a0077e3ee7079e9e1bdcd61f997fe
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-08 13:55:35 +0200

    Lesson 259

--- section-19-advanced-redux/src/App.js
+++ section-19-advanced-redux/src/App.js
@@ -4,7 +4,7 @@ import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useSelector, useDispatch } from "react-redux";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import {sendCartData} from './store/cart-slice'
 
 let isInitial = true;
 
@@ -15,48 +15,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotificaiton({
-          status: "pending",
-          title: "Sending",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(
-        "/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-      dispatch(
-        uiActions.showNotificaiton({
-          status: "success",
-          title: "Success",
-          message: "Sent cart data successfully",
-        })
-      );
-    };
-
-    if(isInitial) {
+    if(isInitial){
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotificaiton({
-          status: "error",
-          title: "Error",
-          message: "Sending cart data failed",
-        })
-      );
-    });
+    
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
