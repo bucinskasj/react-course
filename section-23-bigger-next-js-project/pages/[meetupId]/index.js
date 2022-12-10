2d92cfbb87e032d1d9d18c18e2ee6956ba693457
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-10 18:53:41 +0200

    Lesson 334

---
+++ section-23-bigger-next-js-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail 
+    image='https://images.musement.com/cover/0001/37/vilnius-jpg_header-36634.jpeg'
+    title='First meetup'
+    address='Address'
+    description='Meetup description'
+    />
+  );
+};
+
+export default MeetupDetails;
