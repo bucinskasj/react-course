bc9dfe62ac775c8f835a7f262500f360aae6806f
Jonas Bucinskas <jonas@bucinskas.lt>
2022-12-11 08:42:40 +0200

    Adding a MUI side navigation

---
+++ next-js-mui-side-nav/pages/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import PermanentDrawerLeft from '../components/PermanentLeftDrawer';
+
+export default function MyApp() {
+  return (
+    <div>
+      <PermanentDrawerLeft/>
+    </div>
+  );
+}
